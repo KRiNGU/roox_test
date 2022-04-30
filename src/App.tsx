574edfdd5d7877b2606d192fda7055b000662121
Alexander Lobashov <alexandr.lobashov@mail.ru>
2022-04-30 10:41:51 +0300

    webpack configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { ClickCounter } from './ClickCounter';
+import './hello.scss';
+
 export const App = () => {
-    return <h1>React Typescript</h1>
-}
+  return (
+    <>
+      <h1 className="container">Test Message to check webpack works</h1>
+      <ClickCounter />
+    </>
+  );
+};
